7d1e5ac66fd575caf5d8e4a990088e8ffe6935de
Phu Hung Dang <phu-hung.dang@epita.fr>
2023-09-12 18:01:34 +0700

    update chat working

--- Chat/app.js
+++ Chat/app.js
@@ -10,6 +10,7 @@ const http = require('http').Server(app)
 
 const userRoute = require('./routes/userRoute');
 const User = require('./models/userModel');
+const Chat = require('./models/chatModel');
 
 app.use('/', userRoute);
 
@@ -39,7 +40,20 @@ usp.on('connection', async function(socket){
         //broadcast online status
         socket.broadcast.emit('getOfflineUser', { userId: userId });
     });
-    
+    //chat implementation
+    socket.on('newChat', function(data){
+        socket.broadcast.emit('loadNewChat', data);
+    });
+
+    //load old chat
+    socket.on('existsChat', async function(data){
+        var chats = await Chat.find({ $or:[
+            { sender_id: data.sender_id, receiver_id: data.receiver_id },
+            { sender_id: data.receiver_id, receiver_id: data.sender_id },
+        ]});
+
+        socket.emit('loadChats', { chats: chats });
+    });
 });
 
 http.listen(3000, function(){
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ChatTest
+
+Open the Project
+
+In a terminal run npm start
+
+Connect a MongoDB server at localhost:27017
+
+In a navigator type http://127.0.0.1:3000/register
+
+From there you can open an another navigator to start using the app
